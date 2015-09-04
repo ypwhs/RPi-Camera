79411af096713cd01053acc7423a259e8cc3f089
ypw <915505626@qq.com>
2015-09-05 00:24:41 +0800

    1

--- test5.py
+++ test5.py
@@ -4,25 +4,63 @@ import cv2
 import cv2.cv as cv
 import PIL.Image as Image
 import StringIO
+import socket
+import threading
 
-capture = cv2.VideoCapture(-1)
+capture = cv2.VideoCapture(0)
 capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
 
+
+class Reader(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client = client
+        print self.client
+
+    def run(self):
+        # while True:
+        print "Send jpeg"
+        self.client.sendall(jpeg)
+        self.client.close()
+        print "Finished jpeg"
+
+
+class Listener(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', 8080))
+        self.sock.listen(0)
+
+    def run(self):
+        print("listener started")
+        while True:
+            client = self.sock.accept()
+            print("accept a connect")
+            reader = Reader(client[0])
+            reader.setDaemon(True)
+            reader.start()
+
+listener = Listener()
+listener.setDaemon(True)
+listener.start()
+
 while True:
-    ret, img = capture.read()   # 从摄像头读取图片
-    cv2.imshow('Video', img)    # 显示图片
-    key = cv2.waitKey(30)&0xFF
-    if key == 27:   # ESC
+    ret, img = capture.read()  # 从摄像头读取图片
+    cv2.imshow('Video', img)  # 显示图片
+    key = cv2.waitKey(30) & 0xFF
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 将图片从BGR转为RGB
+    image = Image.fromarray(img)  # 将图片转为Image对象
+    buf = StringIO.StringIO()  # 生成StringIO对象
+    image.save(buf, format="JPEG")  # 将图片以JPEG格式写入StringIO
+    jpeg = buf.getvalue()  # 获取JPEG图片内容
+    buf.close()  # 关闭StringIO
+    if key == 27:  # ESC
         break
     elif key == 32:  # Space
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # 将图片从BGR转为RGB
-        image = Image.fromarray(img)    # 将图片转为Image对象
-        buf = StringIO.StringIO()   # 生成StringIO对象
-        image.save(buf, format="JPEG")  # 将图片以JPEG格式写入StringIO
-        jpeg = buf.getvalue()   # 获取JPEG图片内容
-        buf.close()     # 关闭StringIO
-        print '文件大小：', len(jpeg)/1024, 'KB'
+        print 'File Size:', len(jpeg) / 1024, 'KB'
         f = open('a.jpg', 'w')
         f.write(jpeg)
         f.close()
