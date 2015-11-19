de3ff239d013583dc0570996c62ff163019ec1c7
杨培文 <915505626@qq.com>
2015-11-19 09:47:42 +0800

    1

--- test4.py
+++ test4.py
@@ -1,10 +1,15 @@
 import cv2
+import cv2.cv as cv
 
 capture = cv2.VideoCapture(0)
+capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+capture.set(cv.CV_CAP_PROP_FRAME_COUNT, 100)
 
 while True:
     _, img = capture.read()
     cv2.imshow('Video', img)
-    key = cv2.waitKey(1)
-    # cv2.imwrite('a.jpg', img)
+    key = cv2.waitKey(30) & 0xFF
+    if key == 27:   # ESC
+        break
 capture.release()
--- test5.py
+++ test5.py
@@ -5,9 +5,10 @@ import cv2.cv as cv
 import PIL.Image as Image
 import StringIO
 
-capture = cv2.VideoCapture(-1)
+capture = cv2.VideoCapture(0)
 capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+capture.set(cv.CV_CAP_PROP_FRAME_COUNT, 100)
 
 while True:
     ret, img = capture.read()   # 从摄像头读取图片
